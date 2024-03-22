c1fa0d8acac2bcf8d93416c291bebd1aef07d178
Darthvaderzx <hans.samuelhans@gmail.com>
2024-03-22 13:01:53 +0700

    Improved GET API to show product and customer names

--- Contracts/ResponseModels/Cart/CartDataListResponse.cs
+++ Contracts/ResponseModels/Cart/CartDataListResponse.cs
@@ -9,6 +9,8 @@
         public Guid CartId { get; set; }
         public int Quantity { get; set; }
         public Guid ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
         public Guid CustomerId { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
     }
 }
--- Services/RequestHandlers/ManageCart/CartDataListHandler.cs
+++ Services/RequestHandlers/ManageCart/CartDataListHandler.cs
@@ -17,13 +17,25 @@ namespace Services.RequestHandlers.ManageCart
 
         public async Task<CartDataListResponse> Handle(CartDataListRequest request, CancellationToken cancellationToken)
         {
-            var cartData = await _db.Carts.Select(Q=> new CartDataModel
-            {
-                CartId = Q.CartID,
-                Quantity = Q.Quantity,
-                ProductId = Q.ProductID,
-                CustomerId = Q.CustomerID,
-            }).AsNoTracking().ToListAsync(cancellationToken);
+            //var cartData = await _db.Carts.Select(Q=> new CartDataModel
+            //{
+            //    CartId = Q.CartID,
+            //    Quantity = Q.Quantity,
+            //    ProductId = Q.ProductID,
+            //    CustomerId = Q.CustomerID,
+            //}).AsNoTracking().ToListAsync(cancellationToken);
+            var cartData = await (from c in _db.Carts
+                                  join p in _db.Products on c.ProductID equals p.ProductID
+                                  join cus in _db.Customers on c.CustomerID equals cus.CustomerID
+                                  select new CartDataModel
+                                  {
+                                    CartId = c.CartID,
+                                    Quantity = c.Quantity,
+                                    ProductId = c.ProductID,
+                                    ProductName = p.Name,
+                                    CustomerId = c.CustomerID,
+                                    CustomerName = cus.Name,
+                                  }).AsNoTracking().ToListAsync(cancellationToken);
             if(cartData == null)
             {
                 return new CartDataListResponse();
